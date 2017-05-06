60540a5ed145adf10513f916c04cdd4004e3bd75
Octavian Popa <octavian.popa@windeln.ro>
2017-05-06 11:46:00 +0300

    Removed notification

--- src/main/java/com/notification/NotificationEventType.java
+++
@@ -1,5 +0,0 @@
-package com.notification;
-
-public enum NotificationEventType {
-	Show
-}
--- src/main/java/com/notification/NotificationListener.java
+++
@@ -1,7 +0,0 @@
-package com.notification;
-
-import java.util.EventListener;
-
-public interface NotificationListener extends EventListener {
-	void onEvent(NotificationEvent event);
-}
